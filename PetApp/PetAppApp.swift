2e4b84c564920001f5ba51e374fdfa1708ab2a5c
CheetahCodes21 <mcpai217@gmail.com>
2026-07-16 12:30:00 +1000

    Initial Commit

---
+++ PetApp/PetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAppApp.swift
+//  PetApp
+//
+//  Created by Maroor Chethan Pai on 16/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct PetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
